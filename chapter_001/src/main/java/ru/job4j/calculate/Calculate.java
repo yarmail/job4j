a9e6de063a04f4e665f404ae79943d15ecfdbd97
yarmail <yarmail@yandex.ru>
2019-01-05 13:57:59 +0300

    1.3. JavaDoc. Code convention[#99091]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
+/**
+* Calculate
+* @autor Yarmail (yarmail@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+
 package ru.job4j.calculate;
 
 public class Calculate {
+
+/**
+* Main
+* @param args - args
+*/
 
 	public static void main (String[] args) {
 		System.out.println ("Hello World");
