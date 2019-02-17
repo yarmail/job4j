99b1af96ebbe35a2be4101d962a2dab33ec65821
yarmail <yarmail@yandex.ru>
2019-02-17 16:44:36 +0300

    4.4. Максимум из трех чисел

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -7,9 +7,25 @@ package ru.job4j.max;
 
 public class Max {
    private int maximum;
+   /**
+    * Метод возвращающий максимум их двух чисел.
+   *
+   * @param first  первое значение
+   * @param second второе значение
+   * @return наибольшее значение.
+   */
+
 
    public int max(int first, int second) {
+      // для вычислений максимума из 2-х чисел используем тернарный вариант оператора if
       maximum = first > second ? first : second;
       return maximum;
+   }
+
+   // в теории результат сравнения первых двух чисел сравниваем с третьим числом
+   // используем перегрузку методов (с одинаковым названием)
+   public int max (int first, int second, int third) {
+      // используем вызов метода в методе вместо одной из переменных (матрешка)
+      return max (max(first, second), third);
    }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -6,10 +6,20 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
 
+    // проверка больший из двух
     @Test
     public void whenFirstLessSecond() {
         Max maxim = new Max();
         int result = maxim.max(1, 2);
         assertThat(result, is(2));
     }
+
+    // проверка больший из трех
+    @Test
+    public void whenTreeMoreTwoAndOne() {
+        Max mxm = new Max();
+        int result = mxm.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+
 }
