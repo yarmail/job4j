9ce64bc63df64c6063d325f230a74523ae67551b
yarmail <yarmail@yandex.ru>
2019-11-08 17:33:17 +0300

    Сортировка User с использованием Comparator[#99107]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,18 +1,53 @@
 package ru.job4j.sort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
-/**
- *  В классе SortUser написать метод
- *  public Set<User> sort (List<User>), который будет
- *  возвращать TreeSet пользователей, отсортированных
- *  по возрасту в порядке возрастания.
- */
 public class SortUser {
 
+    /**
+     * Sort by age
+     * Сортировать по возрасту
+     * Примечание:
+     * TreeSet - самосортирующийся - ему нужны указания,
+     * как сортировать
+     * Для такой сортировки класс User должен
+     * 1 вариант: реализовать класс Comparable и
+     * переопределить метод compareTo
+     * 2 вариант: реализовать класс Comparator
+     * и переопределить метод: compare
+     */
     public Set<User> sort(List<User> list) {
         return new TreeSet<>(list);
+    }
+
+    /**
+     * Sort by name length
+     * Сортировка по длине имени
+     * Для сортировки переопределяем компаратор
+     */
+    public List<User> sortNameLength(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User left, User right) {
+                return Integer.compare(left.getName().length(), right.getName().length());
+            }
+        });
+        return list;
+    }
+
+    /**
+     * Sorting by name and age
+     * Cортировка по имени и по возрасту
+     * Для сортировки переопределяем компаратор
+     */
+    public List<User> sortByNameAge(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User left, User right) {
+                int result = left.getName().compareTo(right.getName());
+                return result != 0 ? result : Integer.compare(left.getAge(), right.getAge());
+            }
+        });
+        return list;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -11,15 +11,17 @@ import static org.hamcrest.core.Is.is;
 
 public class SortUserTest {
 
-    User user1 = new User("Ivan", 10);
-    User user2 = new User("Vovan", 20);
-    User user3 = new User("Kolyan", 50);
     /**
-     * Test public Set<User> sort (List<User>)
+     * Sort by age
+     * Сортировать по возрасту
      * Класс User подготовлен к сортировке по возрасту
      */
     @Test
     public void sortTest() {
+
+        User user1 = new User("Ivan", 10);
+        User user2 = new User("Vovan", 20);
+        User user3 = new User("Kolyan", 50);
 
         List<User> users = new ArrayList<>(3);
         users.add(user2);
@@ -33,6 +35,62 @@ public class SortUserTest {
         expected.add(user1);
         expected.add(user2);
         expected.add(user3);
+
+        assertThat(result, is(expected));
+    }
+    /**
+     * Sort by name length
+     * Сортировка по длине имени
+     * Для сортировки переопределяем компаратор
+     */
+    @Test
+    public void sortNameLengthTest() {
+        User user1 = new User("Ad", 10);
+        User user2 = new User("Ivan", 20);
+        User user3 = new User("Aleksey", 30);
+
+        List<User> users = new ArrayList<>(3);
+        users.add(user2);
+        users.add(user1);
+        users.add(user3);
+
+        SortUser sortUser = new SortUser();
+        List<User> result = sortUser.sortNameLength(users);
+
+        List<User> expected = new ArrayList<>(3);
+        expected.add(user1);
+        expected.add(user2);
+        expected.add(user3);
+
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Sorting by name and age
+     * Cортировка по имени и по возрасту
+     * Для сортировки переопределяем компаратор
+     */
+    @Test
+    public void sortByNameAgeTest() {
+        User user1 = new User("Ivan", 25);
+        User user2 = new User("Ivan", 30);
+        User user3 = new User("Sergey", 20);
+        User user4 = new User("Sergey", 25);
+
+        List<User> users = new ArrayList<>(4);
+        users.add(user2);
+        users.add(user1);
+        users.add(user4);
+        users.add(user3);
+
+        SortUser sortUser = new SortUser();
+        List<User> result = sortUser.sortByNameAge(users);
+
+        List<User> expected = new ArrayList<>(4);
+        expected.add(user1);
+        expected.add(user2);
+        expected.add(user3);
+        expected.add(user4);
 
         assertThat(result, is(expected));
     }
