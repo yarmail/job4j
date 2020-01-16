14db9a8e9873435e980469f4ad030c7df153ea65
yarmail <yarmail@yandex.ru>
2020-01-16 22:34:04 +0300

    Chapter 4 lambda

---
+++ chapter_004/src/main/java/ru/job4j/lambda/Calculator.java
@@ -0,0 +1,110 @@
+package ru.job4j.lambda;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+public class Calculator {
+
+    /**
+     * Этот интерфейс описывает функцию,
+     * принимающую два параметра left, right
+     * и возвращающую значение типа double.
+     */
+    @FunctionalInterface
+    public interface Operation {
+        double calc(int left, int right);
+    }
+
+    /**
+     * Вариант 1
+     * Давайте представим, что мы хотим вывести на экран таблицу
+     * умножения от 0 до 10 для числа 2.
+     * Это метод умножения
+     */
+    public void multipleOne(int start, int finish, int value, Operation op) {
+        for (int index = start; index != finish; index++) {
+            System.out.println(op.calc(value, index));
+        }
+    }
+
+    /**
+     * Вариант 2
+     * Давайте теперь перепишем наш пример за счет
+     * использования встроенных функций.
+     *
+     * Здесь используется BiFunction -
+     * это функция принимает два параметра и возвращает значение.
+     *<Integer, Integer, Double> - первый, второй параметр, возвращаемый тип.
+     *
+     * Consumer<Double> media - принимает значение
+     * и ничего не возвращает.
+     *
+     */
+    public void multiple(int start, int finish, int value,
+                         BiFunction<Integer, Integer, Double> op,
+                         Consumer<Double> media) {
+        for (int index = start; index != finish; index++) {
+            media.accept(op.apply(value, index));
+        }
+    }
+
+    /**
+     * Вариант 1
+     * Здесь в метод multiple качестве параметра
+     * (Operation op)
+     * передается анонимный класс - new Operation,
+     * который по-своему реализует интерфейс Operation
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.multiple(0, 10, 2,
+                new Operation() {
+                    @Override
+                    public double calc(int left, int right) {
+                        return left * right;
+                    }
+                });
+    }
+    */
+
+    /**
+     * Вариант 2
+     * Мы реализуем параметр (Operation op - интерфейс) так:
+     * вместо создания анонимного класса типа Operation
+     * мы используем тот факт, что компилятору известно,
+     * какой именно интерфейс мы будем использовать и
+     * используем лямбда выражения:
+     * С левой стороны - (входящие параметры, через запятую,
+     * без указания типа) -> (операторы, работающие с входящими
+     * параметрами) для обязательной реализации метода
+     *
+
+        public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.multiple(0,10,2,
+                (value, index) -> value * index
+        );
+    }
+    */
+
+    /**
+     * Вариант 3
+     * Поправим метод main.
+     * result -> System.out.println(result)
+     * - описывается интерфейсом Consumer<Double>
+     * Если мы используем один входной параметр, скобки можно опускать.
+     */
+    public static void main3(String[] args) {
+        Calculator calc = new Calculator();
+        calc.multiple(
+                0, 10, 2,
+                (value, index) -> {
+                    double result = value * index;
+                    System.out.printf("Multiple %s * %s = %s %n", value, index, result);
+                    return result;
+                },
+                result -> System.out.println(result)
+        );
+    }
+
+}
---
+++ chapter_004/src/test/java/ru/job4j/lambda/CalculatorTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * В первоначальной версии нашего метода,
+ * мы не могли написать тест, из-за того, что данные выводятся в консоль.
+ * В конечной версии мы изменили ситуацию и можем проверить данные.
+ */
+public class CalculatorTest {
+    @Test
+    public void whenAdd1Until3() {
+        Calculator calc = new Calculator();
+        List<Double> buffer = new ArrayList<>();
+        calc.multiple(
+                0, 3, 1,
+                (value, index) -> (double) value + index,
+                result -> buffer.add(result)
+        );
+        assertThat(buffer, is(Arrays.asList(1D, 2D, 3D)));
+    }
+}
