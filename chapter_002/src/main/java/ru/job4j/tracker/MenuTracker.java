c128837bbe7bcff026414878b0ed9c890d23821a
yarmail <yarmail@yandex.ru>
2020-01-31 22:46:45 +0300

    2. В классе StartUI заменить вывод в консоль на Consumer.[#99015] Не могу инициализировать переменную output в классе MenuTracker

--- chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/MenuTracker.java
@@ -2,11 +2,13 @@ package ru.job4j.tracker;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class MenuTracker {
 
     private Input input;
     private Tracker tracker;
+    private final Consumer<String> output;
     /**
      * хранит ссылку на массив типа UserAction.
      */
@@ -15,9 +17,10 @@ public class MenuTracker {
     /**
      * Конструктор 1
      */
-    public MenuTracker(Input input, Tracker tracker) {
+    public MenuTracker(Input input, Tracker tracker, Consumer<String> output) {
         this.input = input;
         this.tracker = tracker;
+        this.output = output;
     }
 
     /**
@@ -42,7 +45,7 @@ public class MenuTracker {
     /**
      * Метод в зависимости от указанного ключа, выполняет соответcтвующее действие.
      *
-     * @param key ключ операции
+     * @param key ключ операции+
      */
     public void select(int key) {
         this.actions.get(key).execute(this.input, this.tracker);
@@ -54,7 +57,7 @@ public class MenuTracker {
     public void show() {
         for (UserAction action : this.actions) {
             if (action != null) {
-                System.out.println(action.info());
+                output.accept(action.info());
             }
         }
     }
--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.function.Consumer;
+
 /**
  * Class StartUI entry point to the program
  * Класс StartUI точка входа в программу, т.е. должен иметь метод main.
@@ -9,19 +11,24 @@ package ru.job4j.tracker;
  *
  * private final Input input;// Получение данных от пользователя.
  * private final Tracker tracker;// Хранилище заявок.
+ *
+ * В новой версии мы выводим данные в буфер с помощью Consumer
+ * также, как мы делали с вводом (с помощью ConsoleInput и StubInput)
  */
 
 public class StartUI {
     private final Input input;
     private final Tracker tracker;
+    private final Consumer<String> output;
 
-    public StartUI(Input input, Tracker tracker) {
+    public StartUI(Input input, Tracker tracker, Consumer<String> output) {
         this.input = input;
         this.tracker = tracker;
+        this.output = output;
     }
 
     public void init() {
-        MenuTracker menu = new MenuTracker(this.input, this.tracker);
+        MenuTracker menu = new MenuTracker(this.input, this.tracker, this.output);
         menu.fillActions();
         do {
             menu.show();
@@ -36,6 +43,6 @@ public class StartUI {
      *
      */
     public static void main(String[] args) {
-        new StartUI(new ValidateInput(new ConsoleInput()), new Tracker()).init();
+        new StartUI(new ValidateInput(new ConsoleInput()), new Tracker(), System.out::println).init();
     }
 }
