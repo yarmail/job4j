c7c2d236ffcf022c5526b0fbd1220a695647a62d
yarmail <yarmail@yandex.ru>
2020-02-17 11:57:25 +0300

    update Chapter 3

--- chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -1,7 +1,7 @@
 package ru.job4j.list;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ConvertMatrix2List {
     public List<Integer> toList(int[][] array) {
