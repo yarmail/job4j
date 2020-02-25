d9a17fd53a69e920e341d0d528486f65990c159a
yarmail <yarmail@yandex.ru>
2020-02-25 18:59:20 +0300

    streamlisttohashmap 1 версия

---
+++ chapter_004/src/main/java/ru/job4j/streamlisttohashmap/package-info.java
@@ -0,0 +1,19 @@
+package ru.job4j.streamlisttohashmap;
+
+/**
+ * 2. Написать программу преобразования List в Map.[#99122]
+ * Использовать Stream.
+ * Исходный вариант:
+ */
+/*
+public class UserConvert {
+
+    public HashMap<Integer, User> process(List<User> list) {
+        HashMap<Integer, User> result = new HashMap<>();
+        for (User user : list) {
+            result.put(user.getId(), user);
+        }
+        return result;
+    }
+}
+*/
