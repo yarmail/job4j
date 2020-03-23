8614f28ea01ee403a0e875d223c9c1452c606123
yarmail <yarmail@yandex.ru>
2020-03-23 20:11:53 +0300

    Встроенная сортировка List

---
+++ chapter_003/src/main/java/ru/job4j/sortlist/ListSort.java
@@ -0,0 +1,21 @@
+package ru.job4j.sortlist;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * В Java большинство коллекций поддерживает
+ * метод Collections.sort, который позволяет
+ * отсортировать коллекцию.
+ *
+ * Давайте рассмотрим это c коллекцией java.utils.List.
+ */
+public class ListSort {
+
+    public static List<Integer> sortAscending() {
+        List<Integer> list = Arrays.asList(5, 3, 4, 1, 2);
+        Collections.sort(list);
+        return list;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/sortlist/ListSortTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.sortlist;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class ListSortTest {
+    List<Integer> expext = Arrays.asList(1, 2, 3, 4, 5);
+
+    @Test
+    public void sortAscending() {
+        List<Integer> result = ListSort.sortAscending();
+        assertThat(result, is(this.expext));
+    }
+}
