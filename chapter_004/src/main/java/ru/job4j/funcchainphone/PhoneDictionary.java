546d6f259524235ab5c8afd608971a993c9d88e8
yarmail <yarmail@yandex.ru>
2020-03-31 18:09:39 +0300

    funcchainphone update

--- chapter_004/src/main/java/ru/job4j/funcchainphone/PhoneDictionary.java
+++ chapter_004/src/main/java/ru/job4j/funcchainphone/PhoneDictionary.java
@@ -1,7 +1,5 @@
 package ru.job4j.funcchainphone;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
@@ -26,14 +24,13 @@ public class PhoneDictionary {
      */
     public ArrayList<Person> find(String key) {
 
-        Predicate<Person> pred1 = i -> i.getAddress().contains(key);
-        Predicate<Person> pred2 = i -> i.getName().contains(key);
-        Predicate<Person> pred3 = i -> i.getPhone().contains(key);
-        Predicate<Person> pred4 = i -> i.getSurname().contains(key);
-        List<Predicate<Person>> predicates = Arrays.asList(pred1, pred2, pred3, pred4);
+        Predicate<Person> p1 = i -> i.getAddress().contains(key);
+        Predicate<Person> p2 = i -> i.getName().contains(key);
+        Predicate<Person> p3 = i -> i.getPhone().contains(key);
+        Predicate<Person> p4 = i -> i.getSurname().contains(key);
 
         return persons.stream()
-                .filter(predicates.stream().reduce(i -> false, Predicate::or))
+                .filter(p1.or(p2).or(p3).or(p4))
                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
     }
 }
--- chapter_004/src/main/java/ru/job4j/funcfilter/package-info.java
+++ chapter_004/src/main/java/ru/job4j/funcfilter/package-info.java
@@ -19,3 +19,22 @@ package ru.job4j.funcfilter;
  * class FilterTest
  *
  */
+
+/*
+Альтернативный вариант использования
+нескольких условий в классе
+
+    public ArrayList<Person> find(String key) {
+
+        Predicate<Person> pred1 = i -> i.getAddress().contains(key);
+        Predicate<Person> pred2 = i -> i.getName().contains(key);
+        Predicate<Person> pred3 = i -> i.getPhone().contains(key);
+        Predicate<Person> pred4 = i -> i.getSurname().contains(key);
+        List<Predicate<Person>> predicates = Arrays.asList(pred1, pred2, pred3, pred4);
+
+        return persons.stream()
+                .filter(predicates.stream().reduce(i -> false, Predicate::or))
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+    }
+
+ */
