4e65cab0da50c5d378508840fdfaaca67204474a
yarmail <yarmail@yandex.ru>
2020-04-06 12:46:20 +0300

    ConvertMatrix2List

--- chapter_004/src/main/java/ru/job4j/streamarrtolist/ConvertMatrix2List.java
+++ chapter_004/src/main/java/ru/job4j/streamarrtolist/ConvertMatrix2List.java
@@ -3,6 +3,8 @@ package ru.job4j.streamarrtolist;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * На основе задачи из 3 части
@@ -17,10 +19,23 @@ import java.util.ArrayList;
  */
 
 public class ConvertMatrix2List {
+
+    /**
+     * 1 вариант
+     */
     public List<Integer> toList(int[][] array) {
         return Arrays.stream(array)
                 .flatMapToInt(Arrays::stream)
                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+    }
+
+    /**
+     * 2 вариант
+     */
+    public static List<Integer> convertToList (Integer[][] martix) {
+        return Stream.of(martix)
+                .flatMap(Stream::of)
+                .collect(Collectors.toList());
     }
 
 }
