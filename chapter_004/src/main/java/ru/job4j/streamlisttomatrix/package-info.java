33689a7777a26925aa7e96d5b4676168ffe14d64
yarmail <yarmail@yandex.ru>
2020-09-13 12:55:33 +0300

    streamlisttomatrix update

--- chapter_004/src/main/java/ru/job4j/streamlisttomatrix/package-info.java
+++ chapter_004/src/main/java/ru/job4j/streamlisttomatrix/package-info.java
@@ -2,5 +2,10 @@ package ru.job4j.streamlisttomatrix;
 
 /**
  *  Конверитировать
- *  List<Integer>  в  int[][]
+ *  List<Integer>  в  int[]
+ *
+ *  Надо делать что-то из этой серии:
+ *  String[] number = collection.stream()
+ *  .flatMap((p) -> Arrays.asList(p.split(",")).stream())
+ *  .toArray(String[]::new);
  */
