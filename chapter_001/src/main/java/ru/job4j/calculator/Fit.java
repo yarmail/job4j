431f4fdb6f10391e89e73efde89fbddf53894957
yarmail <yarmail@yandex.ru>
2021-07-06 19:50:20 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

--- chapter_001/src/main/java/ru/job4j/calculator/Fit.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -1,25 +1,29 @@
 package ru.job4j.calculator;
 
 /**
- * Программа расчета идеального веса для мужчин и женщин
  * The program of calculating the ideal weight for men and women
+ * Программа расчета идеального веса для мужчин и женщин
+ * (сделаны тесты)
  */
 
 public class Fit {
     /**
      * manWeight - идеальный вес для мужчины
-     * @param height
-     * @return
      */
-    public double manWeight(double height) {
+    public static double manWeight(short height) {
         return (height - 100) * 1.15;
     }
     /**
      * womanWeight - идеальный вес для женщины
-     * @param height
-     * @return
      */
-    public double womanWeight(double height) {
+    public static double womanWeight(short height) {
         return (height - 110) * 1.15;
     }
+
+    public static void main(String[] args) {
+        short height = 180;
+        double man = Fit.manWeight(height);
+        System.out.println("Man 180 is " + man);
+    }
+
 }
--- chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
@@ -1,6 +1,29 @@
 package ru.job4j.calculator;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+public class FitTest {
+
+    @Test
+    public void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman170Then69() {
+        short in = 170;
+        double expected = 69;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
+
+/* решение и округление с помощью org.hamcrest.number.IsCloseTo.closeTo
+
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.number.IsCloseTo.closeTo;
 
@@ -21,3 +44,4 @@ public class FitTest {
         assertThat(weight, closeTo(69.0, 0.1));
     }
 }
+*/
