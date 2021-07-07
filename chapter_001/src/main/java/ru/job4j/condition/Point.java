c297c196420f68ce03d73f3e812789bdd8f194ce
yarmail <yarmail@yandex.ru>
2021-07-07 16:30:36 +0300

    3. Тесты для определения расстояния между точками. [#159459]update

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -4,6 +4,7 @@ package ru.job4j.condition;
  * Distance between points in a plane coordinate system
  * Расстояние между точками в плоской системе координат
  * Выглядит примерно так = кв. корень (x2-x1)*2 + (y2-y1)*2
+ * (тесты сделаны)
  */
 public class Point {
     public static double distance(int x2, int x1, int y2, int y1) {
