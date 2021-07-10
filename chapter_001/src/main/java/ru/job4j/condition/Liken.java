f612a4eb489ebc84e56855746d50b3619e1fe5c8
yarmail <yarmail@yandex.ru>
2021-07-10 16:48:11 +0300

    0. Операции сравнения [#235631]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+        System.out.println(first > second);
+        System.out.println(first < second);
+        System.out.println(first == second);
+    }
+}
