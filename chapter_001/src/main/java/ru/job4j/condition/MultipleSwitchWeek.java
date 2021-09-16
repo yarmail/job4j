952d839dad271c60f6979a5cd1ff6df5c30a6ded
yarmail <yarmail@yandex.ru>
2021-09-16 12:07:01 +0300

    4.6.2. Switch - matcher [#334071 #200377]

--- chapter_001/src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -12,6 +12,9 @@ package ru.job4j.condition;
  * Обратите внимание, что switch пишется после =,
  * это необходимое требование при использовании ->
  *
+ * Для выполнения данного задания нужна
+ * SDK не ниже 14 и свежая Idea с
+ * Project language level не ниже "14 - Switch expressions"
  */
 public class MultipleSwitchWeek {
 
@@ -23,13 +26,25 @@ public class MultipleSwitchWeek {
      * так и на русском. Ваша задача - вернуть
      * номер дня недели
      */
-    /*
     public static int numberOfDay(String name) {
     return switch (name) {
         case "Понедельник", "Monday" -> 1;
         case "Вторник", "Tuesday" -> 2;
+        case "Среда", "Wednesday" -> 3;
+        case "Четверг", "Thursday" -> 4;
+        case "Пятница", "Friday" -> 5;
+        case "Суббота", "Saturday" -> 6;
+        case "Воскресенье", "Sunday" -> 7;
         default -> -1;
         };
     }
-    */
+
+    /**
+     * Проверка
+     */
+    public static void main(String[] args) {
+        System.out.println(numberOfDay("Monday")); //1
+        System.out.println(numberOfDay("Пятница")); //5
+    }
+
 }
