a2d50a28bfd31dfaa867baf59a53b345e174a76e
yarmail <yarmail@yandex.ru>
2021-09-17 15:19:28 +0300

    5.5. Простое число [#171694]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+/**
+ * 5.5. Простое число [#171694]
+ * Оператор прерывания цикла - break
+ * (тесты есть)
+ */
+public class CheckPrimeNumber {
+    /**
+     * Странное упрощение предлагает IDEA
+     * Изначально было:
+     * primeNumber = true;
+     * if (number = 1) -> primeNumber = false
+     * Стало:
+     * boolean primeNumber = number != 1;
+     */
+    public static boolean check(int number) {
+        boolean primeNumber = number != 1;
+        for (int index = 2; index < number; index++) {
+            if (number % index == 0) { //если остаток от деления равен 0
+                primeNumber = false;
+                break;
+            }
+        }
+        return primeNumber;
+    }
+}
