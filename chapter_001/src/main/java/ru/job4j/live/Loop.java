0def0c64a4c581e592bb56d651bb1ee98d9e514f
yarmail <yarmail@yandex.ru>
2021-09-17 18:53:13 +0300

    5.9.1. Генерация конструкций в IDEA [#279134]

---
+++ chapter_001/src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,22 @@
+package ru.job4j.live;
+
+/**
+ * 5.9.1. Генерация конструкций в IDEA [#279134]
+ * В этом уроке мы познакомимся с удобной функцией
+ * IDEA - живыми шаблонами.
+ * Живые шаблоны - это набор символов, введенных в поле редактирования,
+ * которые преобразовываются в конструкции языка.
+ */
+public class Loop {
+
+    /**
+     * psvm - public static void main(String[] args)
+     */
+    public static void main(String[] args) {
+        // fori - for (int i = 0; i < 10; i++
+        for (int i = 0; i < 10; i++) {
+            // sout - System.out.println();
+            System.out.println("1");
+        }
+    }
+}
