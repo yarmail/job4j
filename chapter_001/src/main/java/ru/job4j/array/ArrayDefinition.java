c5712636ae158473681dfc40819e1f01506a45f7
yarmail <yarmail@yandex.ru>
2021-09-17 21:03:31 +0300

    6.0.2. Заполнение массива. [#226300]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,8 +1,11 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 /**
  * 6.0.1. Объявление массива. [#3613 #4049]
  * 6.0.1.1. Размер массива [#333582]
+ * 6.0.2. Заполнение массива. [#226300]
  */
 public class ArrayDefinition {
 
@@ -13,5 +16,23 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        /*
+        6.0.2. Заполнение массива. [#226300]
+        Задание
+        2. Создайте внутри метода main массив String на 4 ячейки.
+        Дайте имя переменной созданного массива - names.
+        3. Заполните массив произвольными именами.
+        Заполнение производить через обращение
+        по индексу.
+        4. После этого выведите каждую ячейку на консоль.
+        */
+        String[] names = new String[4];
+        names[0] = "Ivan 1";
+        names[1] = "Ivan 2";
+        names[2] = "Ivan 3";
+        names[3] = "Ivan 4";
+        Arrays.stream(names).
+                forEach(el -> System.out.print(el + " "));
     }
 }
