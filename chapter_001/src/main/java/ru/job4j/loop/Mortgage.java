3b4cb84ef3c6e709808e536205ad8b2081f57a57
yarmail <yarmail@yandex.ru>
2021-09-19 15:53:36 +0300

    5.7. Ипотека [#156315 #204482] update

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -15,7 +15,7 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            year += 1;
+            year ++;
             amount +=  amount * percent / 100 - salary;
         }
         return  year;
