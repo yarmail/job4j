53ad325749718e9bd9e7a83ec23cb6c16bb87129
yarmail <yarmail@yandex.ru>
2021-09-22 21:21:19 +0300

    6.4.1.1. Обход массива с последнего элемента [#361619]

---
+++ chapter_001/src/main/java/ru/job4j/array/ReverseLoopForArray.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+/**
+ * 6.4.1.1. Обход массива с последнего элемента [#361619]
+ * Два варианта:
+ * for (int i = numbers.length - 1; i >=0 ; i--)
+ * или
+ * прямой порядок, но другое обращение к элементам
+ * numbers[numbers.length - 1 - i]
+ */
+public class ReverseLoopForArray {
+
+    /**
+     * Задание
+     * В цикле мы используем прямой проход,
+     * однако элементы выводятся,
+     * начиная с последнего элемента в массиве.
+     * Ваша задача - добавить в цикл проверку,
+     * чтобы в консоль выводились только
+     * элементы с четными индексами.
+     */
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[numbers.length - 1 - i] % 2 == 0) {
+                System.out.println(numbers[numbers.length - 1 - i]);
+            }
+        }
+    }
+}
