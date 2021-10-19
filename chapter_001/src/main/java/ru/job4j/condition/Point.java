ae475bf673e0cc8348c1700ba909a0b7657b146b
yarmail <yarmail@yandex.ru>
2021-10-19 22:52:04 +0300

    Point update

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -18,37 +18,3 @@ public class Point {
         System.out.println(Point.distance(5, 6, 7, 8));
     }
 }
-
-/* Старое решение чарез два объекта с использованием this и that
-
-public class Point {
-    private int x;
-    private int y;
-
-    public Point(int x, int y) {
-    this.x = x;
-    this.y = y;
-    }
-
-    public double distanceTo(Point that) {
-        Point a = this;
-        Point b = that;
-        double result = Math.sqrt(
-                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
-        );
-        return result;
-    }
-
-    public static void main(String[] args) {
-        Point a = new Point(0, 1);
-        Point b = new Point(2, 5);
-        System.out.println("x1 = " + a.x);
-        System.out.println("y1 = " + a.y);
-        System.out.println("x2 = " + b.x);
-        System.out.println("y2 = " + b.y);
-        double result = a.distanceTo(b);
-        System.out.println("Расстояние между точками А и B : " + result);
-    }
-}
-
-*/
