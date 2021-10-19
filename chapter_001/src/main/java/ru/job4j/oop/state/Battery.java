a557594ee7db29868650f2d4990caf16209c96f9
yarmail <yarmail@yandex.ru>
2021-10-19 22:42:44 +0300

    1.8. Состояние объекта [#167388]

---
+++ chapter_001/src/main/java/ru/job4j/oop/state/Battery.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop.state;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+    }
+}
