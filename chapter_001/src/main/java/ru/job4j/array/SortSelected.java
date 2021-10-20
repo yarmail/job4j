3f22a68af4fc899897a6b24d492e3aaf7685a142
yarmail <yarmail@yandex.ru>
2021-10-20 11:47:18 +0300

    comment update

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -43,7 +43,6 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
-            // находим индекс этого числа
             int index = FindLoop.indexOf(data, min, i, data.length - 1);
             SwitchArray.swap(data, i, index);
         }
--- chapter_001/src/main/java/ru/job4j/live/Loop.java
+++ chapter_001/src/main/java/ru/job4j/live/Loop.java
@@ -13,7 +13,7 @@ public class Loop {
      * Часто используемые сокращения
      * psvm - public static void main(String[] args)
      * fori - for (int i = 0; i < 10; i++
-     * // sout - System.out.println();
+     * sout - System.out.println();
      */
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
