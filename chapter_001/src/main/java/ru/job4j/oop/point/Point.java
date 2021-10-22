dda49342c321a358be8633bb79bf8f9580dc7e54
yarmail <yarmail@yandex.ru>
2021-10-22 19:57:10 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- chapter_001/src/main/java/ru/job4j/oop/point/Point.java
+++ chapter_001/src/main/java/ru/job4j/oop/point/Point.java
@@ -7,15 +7,27 @@ package ru.job4j.oop.point;
  *
  */
 public class Point {
-    private int x;
-    private int y;
+    private int x, y, z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public double distanceTo(Point that) {
-        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double distance2D(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2)
+                + Math.pow(this.y - that.y, 2));
+    }
+
+    public double distance3D(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2)
+                + Math.pow(this.y - that.y, 2)
+                + Math.pow(this.z - that.z, 2));
     }
 }
--- chapter_001/src/test/java/ru/job4j/oop/point/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/oop/point/PointTest.java
@@ -10,10 +10,18 @@ import static org.junit.Assert.assertThat;
 public class PointTest {
 
     @Test
-    public void test1115() {
+    public void distance2D() {
         Point a = new Point(1, 1);
         Point b = new Point(1, 5);
-        double result = a.distanceTo(b);
+        double result = a.distance2D(b);
         assertThat(result, is(4.0));
+    }
+
+    @Test
+    public void distance3D() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 9);
+        double result = a.distance3D(b);
+        assertThat(result, is(9.0));
     }
 }
