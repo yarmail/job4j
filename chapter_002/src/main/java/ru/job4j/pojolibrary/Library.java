51fa2e72c61989e04c9c7c19ff4c207e19c31cce
yarmail <yarmail@yandex.ru>
2021-11-19 22:09:15 +0300

    3. Массивы и модели. [#174892 #230349] up

--- chapter_002/src/main/java/ru/job4j/pojolibrary/Library.java
+++ chapter_002/src/main/java/ru/job4j/pojolibrary/Library.java
@@ -8,8 +8,8 @@ public class Library {
         books[2] = new Book("Clean code", 10);
         books[3] = new Book("book3", 10);
 
-        for (int i = 0; i < books.length; i++) {
-            System.out.println(books[i].getName() + " " + books[i].getCount());
+        for (Book book: books) {
+            System.out.println(book.getName() + " " + book.getCount());
         }
         System.out.println("-------------------------------------------");
 
@@ -22,10 +22,9 @@ public class Library {
         }
         System.out.println("------------------------------------------");
 
-        for (int i = 0; i < books.length; i++) {
-            if (books[i].getName().equals("Clean code")) {
-                System.out.println("Ячейка " + i + " "
-                        + books[i].getName() + " " + books[i].getCount());
+        for (Book book: books) {
+            if (("Clean code").equals(book.getName())) {
+                System.out.println(book.getName() + " " + book.getCount());
             }
         }
     }
