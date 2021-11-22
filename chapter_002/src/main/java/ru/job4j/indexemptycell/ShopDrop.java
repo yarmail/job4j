416d117e1212cd48495154c75605bef9bfb8ac8d
yarmail <yarmail@yandex.ru>
2021-11-22 12:28:49 +0300

    3.2. Удаление моделей из массива. [#310016 #231273]

--- chapter_002/src/main/java/ru/job4j/indexemptycell/ShopDrop.java
+++ chapter_002/src/main/java/ru/job4j/indexemptycell/ShopDrop.java
@@ -8,6 +8,14 @@ package ru.job4j.indexemptycell;
  */
 public class ShopDrop {
     public static Product[] delete(Product[] products, int index) {
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+
+    public static Product[] deleteAlternative(Product[] products, int index) {
         System.arraycopy(products, index + 1, products, index, products.length - 1 - index);
         products[products.length - 1] = null;
         return products;
