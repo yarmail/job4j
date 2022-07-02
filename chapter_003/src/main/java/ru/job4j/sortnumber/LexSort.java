a2ff474fa35b96516ef3c3505db07960044af5e3
yarmail <yarmail@yandex.ru>
2022-07-02 17:11:09 +0300

    4. Сортировка номера [#221284]

---
+++ chapter_003/src/main/java/ru/job4j/sortnumber/LexSort.java
@@ -0,0 +1,24 @@
+package ru.job4j.sortnumber;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+/**
+ * 4. Сортировка номера [#221284]
+ * 1. Реализуйте компаратор LexSort.
+ * Он должен сравнивать номер, а не целиком строку.
+ * (есть тесты, в них примеры)
+ */
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return convert(o1).compareTo(convert(o2));
+    }
+
+    public Integer convert(String string) {
+        return  Stream.of(string.split("\\."))
+                .findFirst()
+                .map(Integer::parseInt)
+                .get();
+    }
+}
