8d93b3499c0a358e903d5d5881fa75ddb728ea8c
yarmail <yarmail@yandex.ru>
2022-07-04 12:19:54 +0300

    4. Сортировка номера [#221284] update

--- chapter_003/src/main/java/ru/job4j/sortnumber/LexSort.java
+++ chapter_003/src/main/java/ru/job4j/sortnumber/LexSort.java
@@ -22,3 +22,14 @@ public class LexSort implements Comparator<String> {
                 .get();
     }
 }
+/*
+Альтернативный вариант
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        int leftValue = Integer.parseInt(left.split(". ")[0]);
+        int rightValue = Integer.parseInt(right.split(". ")[0]);
+        return Integer.compare(leftValue, rightValue);
+    }
+*/
