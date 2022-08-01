180c853743553ab7616985d86ff709b309ce8614
yarmail <yarmail@yandex.ru>
2022-08-01 11:09:29 +0300

    6.9. String - это массив [#504884] new solution

--- chapter_001/src/main/java/ru/job4j/string/JavaNameValidator.java
+++ chapter_001/src/main/java/ru/job4j/string/JavaNameValidator.java
@@ -27,3 +27,28 @@ public class JavaNameValidator {
 a-z, A-Z, 0-9 и знаков "_" и "$"
 
  */
+
+/* Альтернативный вариант
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        char[] symbols = name.toCharArray();
+        if (symbols[0] < 'a' || symbols[0] > 'z') {
+            return false;
+        }
+
+        for (int i = 1; i < symbols.length; i++) {
+            if (!isCorrectSymbol(symbols[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isCorrectSymbol(char symbol) {
+        return symbol >= 'a' && symbol <= 'z'
+                || symbol >= '0' && symbol <= '9'
+                || symbol == '_'
+                || symbol == '$';
+    }
+}
+ */
