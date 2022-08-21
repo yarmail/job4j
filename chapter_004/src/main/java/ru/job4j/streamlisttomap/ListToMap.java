42c513106c0463f59957d7ded97567f563dc0a0c
yarmail <yarmail@yandex.ru>
2022-08-21 22:41:55 +0300

    4. Преобразование List в Map. [#110226]

---
+++ chapter_004/src/main/java/ru/job4j/streamlisttomap/ListToMap.java
@@ -0,0 +1,38 @@
+package ru.job4j.streamlisttomap;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * 4. Преобразование List в Map. [#110226]
+ * Задание.
+ * 1. Класс Student взять из задания
+ * "Фильтрация учеников".
+ * 2. Вашим заданием будет реализовать метод,
+ * который на вход принимает список студентов
+ * List<Student> и вернет результат его преобразования
+ * в Map<String, Student>, где:
+ * ключ - это фамилия студента;
+ * значение - объект ученика.
+ *
+ * Предусмотреть в реализации исключение дубликатов,
+ * поскольку ключ - это фамилия, то дубликатами будут те пары,
+ * у которых совпадают фамилии студентов.
+ * В случае добавления дубликата необходимо
+ * оставить старое значение.
+ *
+ * (есть тесты)
+ */
+
+public class ListToMap {
+    public static Map<String, Student> convert(List<Student> list) {
+        return list.stream()
+                .collect(Collectors.toMap(
+                                Student::getSurname,
+                                e -> e,
+                                (ex, rep) -> ex
+                        )
+                );
+    }
+}
